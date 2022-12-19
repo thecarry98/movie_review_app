efe65372456efeb0d3063bf1a0dc74ba2090ec42
Việt Anh <vietanh030498@gmail.com>
2022-12-19 14:07:50 +0700

    add home demo

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(child: Center(child: Text('haha')));
   }
 }
